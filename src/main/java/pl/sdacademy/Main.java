b7beb0ef2aab36a2efb3b656fced6ef96b243449
DawidD90 <dawid9013@onet.pl>
2022-12-15 20:11:49 +0100

    test

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -5,6 +5,6 @@ package pl.sdacademy;
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world222!");
+    System.out.println("Hello world2222131231!");
   }
 }
