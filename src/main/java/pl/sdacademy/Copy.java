72bbb018e9ed3db0b844f22299bc27169c3fe93f
brzozaKoduje <p.sobecki@op.pl>
2023-01-03 20:15:34 +0100

    Copy update

--- src/main/java/pl/sdacademy/Copy.java
+++ src/main/java/pl/sdacademy/Copy.java
@@ -1,4 +1,32 @@
 package pl.sdacademy;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Copy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcopy")
+    private int idcopy;
+    @Column
+    private String identificator;
+    @Column(name = "borrow_date")
+    private LocalDate borrowDate;
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+
 }
