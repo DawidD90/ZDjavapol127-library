64b6e2f580c81bf6336bfead7f6da564ca9a47cf
Diabel170 <szprotwpuszce@gmail.com>
2023-01-03 19:50:19 +0100

    BooksPush1

--- src/main/java/pl/sdacademy/Books.java
+++ src/main/java/pl/sdacademy/Books.java
@@ -1,4 +1,36 @@
 package pl.sdacademy;
 
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+    @Column
+    private String title;
+    @Column
+    private String publisher; //ewentualnie enum
+    @Column
+    private String isbn;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "Books_Authors",
+            joinColumns = {@JoinColumn(name = "book_id")},
+            inverseJoinColumns = {@JoinColumn(name = "author_id")}
+    )
+    private List<Author> authors;
+
+
+    @OneToMany(mappedBy = "books")
+    private List<Copy> copies;
 }
