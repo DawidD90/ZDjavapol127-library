a67ff2afbd568e32e1c7b9a92af35733d7148647
DawidD90 <dawid9013@onet.pl>
2023-01-03 18:17:03 +0100

    Author update+ pom.xml

--- src/main/java/pl/sdacademy/Author.java
+++ src/main/java/pl/sdacademy/Author.java
@@ -1,4 +1,16 @@
 package pl.sdacademy;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
